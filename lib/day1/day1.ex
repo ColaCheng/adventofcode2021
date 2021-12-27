74b182fa7503c915dc09eeeb72addb8da83c9a1c
Cola Cheng <imlikedog@gmail.com>
2021-12-27 12:21:44 +0800

    Day1 part2.

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -31,4 +31,31 @@ defmodule Day1 do
   defp count_larger_than_previous_measurement(now, {_last, acc}) do
     {now, acc}
   end
+
+  def run2() do
+    {:ok, file} = File.open(@input_file, [:read])
+
+    IO.read(file, :line)
+    |> read_line_reduce(file, {[], 0}, &count_larger_than_previous_3sum_measurement/2)
+    |> elem(1)
+  end
+
+  defp count_larger_than_previous_3sum_measurement(now, {previous, acc})
+       when length(previous) < 3 do
+    {[now | previous], acc}
+  end
+
+  defp count_larger_than_previous_3sum_measurement(now, {previous, acc}) do
+    last = Enum.sum(previous)
+    new_previous = [now | Enum.take(previous, 2)]
+    current = Enum.sum(new_previous)
+
+    new_acc =
+      case current > last do
+        true -> acc + 1
+        false -> acc
+      end
+
+    {new_previous, new_acc}
+  end
 end
