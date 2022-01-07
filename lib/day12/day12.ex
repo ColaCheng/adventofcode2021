49c639826ec11fed19b4b603b86a57b80f95855b
Cola Cheng <imlikedog@gmail.com>
2022-01-07 23:14:54 +0800

    Day12 part2.

--- lib/day12/day12.ex
+++ lib/day12/day12.ex
@@ -13,6 +13,18 @@ defmodule Day12 do
     |> length()
   end
 
+  def run2() do
+    {:ok, file} = File.open(@input_file, [:read])
+
+    IO.read(file, :line)
+    |> read_line_reduce(file, [], &process/2)
+    |> Enum.uniq()
+    |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
+    |> paths(&over_a_small_cave_visit_twice?/1)
+    |> resolve([])
+    |> length()
+  end
+
   defp read_line_reduce(:eof, file, acc, _reducer) do
     File.close(file)
     acc
@@ -30,31 +42,34 @@ defmodule Day12 do
     [{from, to}, {to, from} | edges]
   end
 
-  defp paths(%{"start" => caves} = map) do
-    Enum.map(caves, &traverse(&1, map, []))
+  defp paths(%{"start" => caves} = map, extra_fun \\ fn _ -> true end) do
+    Enum.map(caves, &traverse(&1, map, [], extra_fun))
   end
 
-  defp traverse("end", _map, acc) do
+  defp traverse("end", _map, acc, _extra_fun) do
     ["end" | acc]
   end
 
-  defp traverse("start", _map, acc) do
+  defp traverse("start", _map, acc, _extra_fun) do
     acc
   end
 
-  defp traverse(nil, _map, acc) do
+  defp traverse(nil, _map, acc, _extra_fun) do
     acc
   end
 
-  defp traverse(cave, map, acc) do
+  defp traverse(cave, map, acc, extra_fun) do
     nexts = Map.get(map, cave, [])
 
     case {String.downcase(cave), Enum.member?(acc, cave)} do
       {^cave, true} ->
-        acc
+        case extra_fun.(acc) do
+          true -> acc
+          false -> Enum.map(nexts, &traverse(&1, map, [cave | acc], extra_fun))
+        end
 
       _ ->
-        Enum.map(nexts, &traverse(&1, map, [cave | acc]))
+        Enum.map(nexts, &traverse(&1, map, [cave | acc], extra_fun))
     end
   end
 
@@ -72,5 +87,15 @@ defmodule Day12 do
 
   defp resolve([], acc) do
     acc
+  end
+
+  defp over_a_small_cave_visit_twice?(paths) do
+    Enum.frequencies(paths)
+    |> Enum.any?(fn {cave, count} ->
+      case String.downcase(cave) do
+        ^cave when count > 1 -> true
+        _ -> false
+      end
+    end)
   end
 end
